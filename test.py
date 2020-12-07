4a78d0bb7c933a41da67578a18f128fd0636644a
zz21093 <zz21093@gmail.com>
2020-12-07 13:36:55 -0500

    Commit all.

---
+++ test.py
@@ -0,0 +1,15 @@
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('World'))
+print(greet('Joe'))
+
+print('Make some changes.')
---
+++ test2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+n = np.array([1, 2, 3, 4])
+print(n)
+
+a = np.array(['a', 'b', 'c', 'd'])
+print(a)
+
+print('Hello, Joe!')
